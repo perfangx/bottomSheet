b95366d422bcbfa0a5a4d03851f4a78dda2cd79e
Roba gh <rfalghanmi@gmail.com>
2023-05-15 09:34:50 +0300

    Initial Commit

---
+++ buttom sheet/buttom_sheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  buttom_sheetApp.swift
+//  buttom sheet
+//
+//  Created by roba on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct buttom_sheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
